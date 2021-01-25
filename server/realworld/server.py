f33b8b29d9a6ad9b4dda1fbb3b61b0d8befa1089
HSIA CHI <methane0103@gmail.com>
2021-01-25 19:31:18 +0800

    temp commit

--- server/realworld/server.py
+++ server/realworld/server.py
@@ -9,6 +9,7 @@ from threading import Lock
 import pickle
 import numpy as np
 import time
+import math
 from scipy.optimize import leastsq
 from window import Window
 from visualize import DataCollectionVisualizer, LocalizationVisualizer
@@ -21,14 +22,14 @@ WINDOW_SIZE = 10
 PRED_WIN_SIZE = 20
 
 class Server():
-	def __init__(self, energy_func=None):
+	def __init__(self):
 		self.serial_window = Window(WINDOW_SIZE)
 		self.ser = serial.Serial(SERIAL_PORT)
 		self.lock = Lock()
 		self.data = {
 			'quats': [],
 			'radiances': [],
-			'vec': [1, 0, 0]
+			'vec': None
 		}
 		# self.visualizer = DataCollectionVisualizer()
 		self.visualizer = LocalizationVisualizer()
@@ -36,7 +37,7 @@ class Server():
 			'quats': [],
 			'radiances': []
 		}
-		self.energy_func = energy_func
+		self.energy_func = self.__energy_func
 
 	def save_data(self, path=None):
 		if path: 
@@ -69,39 +70,48 @@ class Server():
 		d = np.linalg.norm(vec)
 		vec = vec / d
 		radiances = []
+		t = time.time()
 		for q in quats:
 			mat = quat_to_mat(q)
 			forward_vec = normalized(mat[1])
 			upper_vec = mat[2]
 			sgn = np.sign(np.cross(vec, forward_vec).dot(upper_vec))
 			angle = np.arccos(forward_vec.dot(vec) * sgn)
-			print(angle)
-			radiances.append(self.__energy_func(angle, d))	# TODO: normalize distance in energy function
-		print(quats[-5:], radiances[-5:])
+			radiances.append(self.__energy_func(angle, d))
 		return radiances
 
 	def __residual_func(self, vec, quats, radiances):
-		print(radiances)
-		return (np.array(self.__fit_func(vec, quats)) - np.array(radiances)) ** 2
+		t = time.time()
+		val = (np.array(self.__fit_func(vec, quats)) - np.array(radiances)) ** 2
+		return val
+
 
 	def __recv_data_from_socket(self):
 		quat_window = Window(WINDOW_SIZE)
+		calc_pos_cnt = 0
 
 		while True:	
-			quat_buf = conn.recv(1024)
+			quat_buf = conn.recv(2048)
 			if not quat_buf:
 				break
 
-			# TODO: wait for 10 frames of ir radiance
+			# wait for 10 frames of ir radiance for alignment
+			# l = len(self.raw_data['radiances'])
+			# while len(self.raw_data['radiances']) - l < 10:
+			# 	time.sleep(0.001)
 			with self.lock:
 				ir_radiance = self.serial_window.mean_filter()
 				rad_idx = len(self.raw_data['radiances']) - 1
 
-			multi_quat_list = quat_buf.decode("utf-8").split('\n')[:-1]		# a list of string, each line contains quaternions from multi sources, separated by ';'
-			for multi_quat in multi_quat_list:
-				multi_quat = multi_quat.split(';')[:-1]
-				multi_quat = [[float(dim) for dim in quat.split(' ')] for quat in multi_quat]	# change quat from str to [float, float, float]
-				quat_window.push(multi_quat[0])
+
+			quats = quat_buf.decode("utf-8").split('\n')[:-1]
+			for quat in quats:
+				try:
+					quat = [float(dim) for dim in quat.split(' ')]
+				except:
+					print(quat)
+					print(quats)
+				quat_window.push(quat)
 
 			quat = quat_window.get_last()
 
@@ -109,16 +119,33 @@ class Server():
 				self.data['quats'].append(quat)
 				self.data['radiances'].append(ir_radiance)
 
-			if self.__energy_func and len(self.data['quats']) > PRED_WIN_SIZE:
-				quats = self.data['quats'][-PRED_WIN_SIZE:]
-				radiances = self.data['radiances'][-PRED_WIN_SIZE:]
-				vec = self.data['vec']
-				vec = leastsq(self.__residual_func, np.array(vec), args=(quats, 1023 - np.array(radiances)))[0]
-				print(vec)
-				with self.visualizer.lock:
-					self.data['vec'] = vec
+			calc_pos_cnt += 1
+			if self.energy_func and calc_pos_cnt > 5 and len(self.data['quats']) > PRED_WIN_SIZE:
+				calc_pos_cnt = 0
+				def runner():
+					quats = self.data['quats'][-PRED_WIN_SIZE:]
+					radiances = self.data['radiances'][-PRED_WIN_SIZE:]
+					# if self.data['vec'] is None:
+					# 	vec = quat_to_mat(quats[0])[1]	# initialized as first quat
+					# else:
+					# 	vec = self.data['vec']
+					vec = quat_to_mat(quats[0])[1]
+					t = time.time()
+					vec = leastsq(self.__residual_func, np.array(vec), maxfev=50, args=(quats, 1023 - np.array(radiances)))[0]
+					# print('elapsed time: ', time.time() - t)
+					if self.data['vec'] is not None:
+						print('Radiance with current vec:', self.__fit_func(self.data['vec'], quats)[0])
+					print('radiance: ', 1023 - radiances[0])
+					# print('vec: ', vec)
+					if math.isnan(vec[0]) or math.isnan(vec[1]) or math.isnan(vec[2]):
+						vec = None
+					with self.visualizer.lock:
+						self.data['vec'] = vec
+					self.visualizer.update_data(self.data)
+				th = threading.Thread(target=runner)
+				th.start()
 
-			self.visualizer.update_data(self.data)
+			# self.visualizer.update_data(self.data)
 
 			self.raw_data['quats'].append([quat, rad_idx])
 			self.raw_data['radiances'].append(ir_radiance)
